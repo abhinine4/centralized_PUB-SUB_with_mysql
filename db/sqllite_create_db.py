cfd686a091139d0728d408b0d429fd26e3d37661
Abhishek <abhisheknine4@gmail.com>
2021-10-22 22:54:14 -0400

    adding local db

--- db/sqllite_create_db.py
+++ db/sqllite_create_db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 conn = sqlite3.connect('pubsub.db')
 c = conn.cursor()
-#
+
 # c.execute("DROP table eventBroker")
 # c.execute("DROP table publisher")
 # c.execute("DROP table subscriber")
@@ -29,37 +29,6 @@ c = conn.cursor()
 #         notification INTEGER DEFAULT 0)
 #         """)
 
-
-############################################################################
-# queries
-
-# c.execute("UPDATE eventBroker set eventData = ? WHERE eid = ?", (ev1,1))
-# c.execute("SELECT * from eventBroker")
-# print(c.fetchall())
-
-# pid = 'asdasd'
-# eid = 3
-# c.execute("INSERT INTO publisher VALUES(?,?,?)",(pid,eid,1))
-# c.execute("UPDATE publisher SET advertisement = 0 WHERE pid = ? AND eid = ?", (pid, eid))
-# c.execute("SELECT * FROM publisher")
-# print(c.fetchall())
-
-# sid1 = 'aaaa'
-# sid2 = 'bbbb'
-# sid3 = 'cccc'
-# eid1 = 1
-# eid2 = 2
-# c.execute("INSERT INTO SUBSCRIBER VALUES(?,?,?,?)", (sid1, 1, 0, 0))
-# c.execute("INSERT INTO SUBSCRIBER VALUES(?,?,?,?)", (sid2, 2, 0, 0))
-# c.execute("INSERT INTO SUBSCRIBER VALUES(?,?,?,?)", (sid2, 1, 0, 0))
-#
-# c.execute("UPDATE subscriber SET notification = 1 WHERE eid = ?", str(eid2))
-# c.execute("UPDATE subscriber SET notification = 1 WHERE eid = ?", str(eid1))
-# c.execute("SELECT DISTINCT * FROM subscriber WHERE sid = ? AND notification = 1", (sid2,))
-# print(c.fetchall())
-
 conn.commit()
 conn.close()
 
-
-#######################################################################
